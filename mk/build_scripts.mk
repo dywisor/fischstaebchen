9340db9f9b345cd1e991bafe57e7b9263775a009
André Erdmann <dywi@mailerd.de>
2015-03-28 23:21:08 +0100

    mk: split up build_scripts.mk

--- mk/build_scripts.mk
+++ mk/build_scripts.mk
@@ -3,30 +3,3 @@ $(error __PRJROOT is not set)
 endif
 
 BUILDSCRIPTS_DIR := $(__PRJROOT)/scripts
-
-# shellcheck
-X_SHELLCHECK = $(BUILDSCRIPTS_DIR)/run-shellcheck
-
-# metash
-X_METASH = $(BUILDSCRIPTS_DIR)/metash
-
-METASH_VDEF_OPTS = -F '$(__PRJROOT)/misc/metash_vdef'
-ifneq ($(METASH_DEFAULTS_FILE),)
-METASH_VDEF_OPTS += -F '$(METASH_DEFAULTS_FILE)'
-endif
-
-METASH_VDEF_OPTS += $(foreach f,$(METASH_DEFAULTS_FILES),-F '$(f)')
-
-RUN_METASH = $(X_METASH) $(METASH_VDEF_OPTS) $(METASH_OPTS)
-
-# f_run_metash_convert_dir ( srcdir, outdir, ["--stdin"], [extra_opts] )
-define f_run_metash_convert_dir
-	$(SHELL) $(BUILDSCRIPTS_DIR)/run-metash $(3) \
-		'$(strip $(1))' '$(strip $(2))' -- $(RUN_METASH) $(4)
-endef
-
-# f_run_metash_convert_dir_ifexist ( srcdir, outdir, ["--stdin"], [extra_opts] )
-define f_run_metash_convert_dir_ifexist
-	test ! -e '$(strip $(1))' || \
-		$(call f_run_metash_convert_dir,$(1),$(2),$(3),$(4))
-endef
---
+++ mk/shellgen.mk
@@ -0,0 +1,30 @@
+ifeq ($(BUILDSCRIPTS_DIR),)
+$(error build_scripts must be included before this file)
+endif
+
+# shellcheck
+X_SHELLCHECK = $(BUILDSCRIPTS_DIR)/run-shellcheck
+
+# metash
+X_METASH = $(BUILDSCRIPTS_DIR)/metash
+
+METASH_VDEF_OPTS = -F '$(__PRJROOT)/misc/metash_vdef'
+ifneq ($(METASH_DEFAULTS_FILE),)
+METASH_VDEF_OPTS += -F '$(METASH_DEFAULTS_FILE)'
+endif
+
+METASH_VDEF_OPTS += $(foreach f,$(METASH_DEFAULTS_FILES),-F '$(f)')
+
+RUN_METASH = $(X_METASH) $(METASH_VDEF_OPTS) $(METASH_OPTS)
+
+# f_run_metash_convert_dir ( srcdir, outdir, ["--stdin"], [extra_opts] )
+define f_run_metash_convert_dir
+	$(SHELL) $(BUILDSCRIPTS_DIR)/run-metash $(3) \
+		'$(strip $(1))' '$(strip $(2))' -- $(RUN_METASH) $(4)
+endef
+
+# f_run_metash_convert_dir_ifexist ( srcdir, outdir, ["--stdin"], [extra_opts] )
+define f_run_metash_convert_dir_ifexist
+	test ! -e '$(strip $(1))' || \
+		$(call f_run_metash_convert_dir,$(1),$(2),$(3),$(4))
+endef
