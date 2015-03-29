034939c4221c5dac0932c979ced8847b9ede4a48
André Erdmann <dywi@mailerd.de>
2015-03-29 22:03:23 +0200

    overlay: move newroot fspaths functions from base to newroot

--- overlay/src/base/functions/newroot/20-fsutil/05-prefix.sh
+++
@@ -1,7 +0,0 @@
-## Copyright (c) 2014-2015 André Erdmann <dywi@mailerd.de>
-##
-## Distributed under the terms of the MIT license.
-## (See LICENSE.MIT or http://opensource.org/licenses/MIT)
-##
-
-## void newroot_prefix_fspath ( prefix..., fspath )
