58bce6fa9a55ccc7d104071772091f1e003fcaf0
André Erdmann <dywi@mailerd.de>
2015-09-29 22:46:15 +0200

    overlay/base: create disk-by links
    Create /dev/disk/by-{uuid,label,partuuid,id} links.
    
    Requires external progs:
    * blkid (part of busybox / util-linux)
    * diskid [0]
       (alternative: ata_id from systemd-udevd)
    
    Currently, the disk-by symlinks get created during the "devfs-setup" hook.
    
    [0] https://github.com/dywisor/diskid

---
+++ overlay/src/base/hooks/devfs-setup.d/80-storage_disk_links.sh
@@ -0,0 +1,7 @@
+## Copyright (c) 2014-2015 André Erdmann <dywi@mailerd.de>
+##
+## Distributed under the terms of the MIT license.
+## (See LICENSE.MIT or http://opensource.org/licenses/MIT)
+##
+__HAVE_FISCH_SHELLFUNC__=YES
+runscript_simple_or_die "@@OVSCRIPTS@@/create-devfs-disk-links"
